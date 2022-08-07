cab707aa8c5ef8b73a454edd37c41cbd9247dc87
Bjarne Seibel <48617372+SnakeFist007@users.noreply.github.com>
2022-08-07 13:32:19 +0100

    Implemented __name__

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ token = token_file.read()
 
 ### DEBUG - REMOVE BEFORE PRODUCTIVE RELEASE!!!
 testServerID = env.server_id
-### DEBUG - REMOVE BEFORE PRODUCTIVE RELEASE!!!
 
+# Intents & Bot initialization
 intents = nextcord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
@@ -18,13 +18,18 @@ bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 # Events
 @bot.event
 async def on_ready():
-    print("\n Lene#2184 is ready!")
+    print("\n\tLene#2184 is ready!\n")
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="you <3"))
 
-# Load extensions
-for folder in os.listdir("cogs"):
-    if os.path.exists(os.path.join("cogs", folder, "cog.py")):
-        bot.load_extension(f"cogs.{folder}.cog")
+def main():
+    # Load extensions
+    for folder in os.listdir("cogs"):
+        if os.path.exists(os.path.join("cogs", folder, "cog.py")):
+            bot.load_extension(f"cogs.{folder}.cog")
+    
+    # Start the bot
+    bot.run(token)
 
-# Run
-bot.run(token)
+
+if __name__ == "__main__":
+    main()
