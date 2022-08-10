770ba5b4b9f00880de115558e69ed2f982d7eaf1
Bjarne Seibel <48617372+SnakeFist007@users.noreply.github.com>
2022-08-10 10:48:35 +0100

    Added admin controls

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from code import interact
 import os
 import env
 import nextcord
+from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
 
 # Variables
@@ -20,6 +22,37 @@ bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 async def on_ready():
     print("\n\tLene#2184 is ready!")
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="you <3"))
+
+
+# Load cogs
+@bot.slash_command(name="load", description="Loads a cog", guild_ids=[testServerID])
+@commands.is_owner()
+async def load_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
+    bot.load_extension(f"cogs.{cog}.cog")
+    await interaction.response.send_message(f"Loaded cog **{cog}**!", ephemeral=True)
+
+# Reload cogs
+@bot.slash_command(name="reload", description="Reloads a cog", guild_ids=[testServerID])
+@commands.is_owner()
+async def reload_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
+    bot.reload_extension(f"cogs.{cog}.cog")
+    await interaction.response.send_message(f"Reloaded cog **{cog}**!", ephemeral=True)
+
+# Unload cogs
+@bot.slash_command(name="unload", description="Unloads a cog", guild_ids=[testServerID])
+@commands.is_owner()
+async def unload_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
+    bot.unload_extension(f"cogs.{cog}.cog")
+    await interaction.response.send_message(f"Unloaded cog **{cog}**!", ephemeral=True)
+
+
+# Shutdown bot
+@bot.slash_command(name="logout", description="[!!!] Shuts down the bot [!!!]", guild_ids=[testServerID])
+@commands.is_owner()
+async def bot_shutdown(interaction: Interaction):
+    await interaction.response.send_message("**SHUTTING DOWN THE BOT**", ephemeral=True)
+    await bot.close()
+
 
 def main():
     # Load extensions
