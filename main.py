272229d2980c1bba624952fd09c1f9fa6f29d3f3
Bjarne Seibel <48617372+SnakeFist007@users.noreply.github.com>
2022-08-13 14:39:44 +0100

    restructure main module

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ async def on_ready():
     print("\n\tLene#2184 is ready!")
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="you <3"))
 
-
+### COG CONTROL
 # Load cogs
 @bot.slash_command(name="load", description="Loads a cog", guild_ids=[testServerID])
 @commands.is_owner()
 async def load_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
     print(f"Loading {cog} module...")
     bot.load_extension(f"cogs.{cog}.cog")
+    
     await interaction.response.send_message(f"Loaded cog **{cog}**!", ephemeral=True)
 
 # Reload cogs
@@ -37,6 +38,7 @@ async def load_cog(interaction: Interaction, cog: str = SlashOption(description=
 async def reload_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
     print(f"Reloading {cog} module...")
     bot.reload_extension(f"cogs.{cog}.cog")
+    
     await interaction.response.send_message(f"Reloaded cog **{cog}**!", ephemeral=True)
 
 # Unload cogs
@@ -46,17 +48,11 @@ async def unload_cog(interaction: Interaction, cog: str = SlashOption(descriptio
     print(f"Unloading {cog} module...")
     bot.unload_extension(f"cogs.{cog}.cog")
     print(f"Unloaded {cog} module!")
+    
     await interaction.response.send_message(f"Unloaded cog **{cog}**!", ephemeral=True)
 
 
-# Shutdown bot
-@bot.slash_command(name="logout", description="[!!!] Shuts down the bot [!!!]", guild_ids=[testServerID])
-@commands.is_owner()
-async def bot_shutdown(interaction: Interaction):
-    await interaction.response.send_message("**SHUTTING DOWN THE BOT**", ephemeral=True)
-    await bot.close()
-
-
+# Main run function
 def main():
     # Load extensions
     for folder in os.listdir("cogs"):
