4f735953eed9f5af5fb3b352ac17349719a8e5a0
Bjarne Seibel <48617372+SnakeFist007@users.noreply.github.com>
2022-08-13 15:05:05 +0100

    removed cog controls

--- main.py
+++ main.py
@@ -14,42 +14,13 @@ testServerID = env.server_id
 # Intents & Bot initialization
 intents = nextcord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
+bot = commands.Bot(intents=intents)
 
 # Events
 @bot.event
 async def on_ready():
     print("\n\tLene#2184 is ready!")
     await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="you <3"))
-
-### COG CONTROL
-# Load cogs
-@bot.slash_command(name="load", description="Loads a cog", guild_ids=[testServerID])
-@commands.is_owner()
-async def load_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
-    print(f"Loading {cog} module...")
-    bot.load_extension(f"cogs.{cog}.cog")
-    
-    await interaction.response.send_message(f"Loaded cog **{cog}**!", ephemeral=True)
-
-# Reload cogs
-@bot.slash_command(name="reload", description="Reloads a cog", guild_ids=[testServerID])
-@commands.is_owner()
-async def reload_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
-    print(f"Reloading {cog} module...")
-    bot.reload_extension(f"cogs.{cog}.cog")
-    
-    await interaction.response.send_message(f"Reloaded cog **{cog}**!", ephemeral=True)
-
-# Unload cogs
-@bot.slash_command(name="unload", description="Unloads a cog", guild_ids=[testServerID])
-@commands.is_owner()
-async def unload_cog(interaction: Interaction, cog: str = SlashOption(description="Choose the cog")):
-    print(f"Unloading {cog} module...")
-    bot.unload_extension(f"cogs.{cog}.cog")
-    print(f"Unloaded {cog} module!")
-    
-    await interaction.response.send_message(f"Unloaded cog **{cog}**!", ephemeral=True)
 
 
 # Main run function
