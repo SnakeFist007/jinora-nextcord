766651575ad68f29bb0768b96205fd116b5966b3
SnakeFist007 <github@bjarne-seibel.de>
2023-07-20 23:19:01 +0200

    Further Cleanup

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import os
-import shutil
 import logging
-import aiosqlite
 import nextcord
 from nextcord.ext import commands
 from dotenv import load_dotenv
 
 ## Variables
 load_dotenv()
-db_servers = "database\servers.db"
-db_characters = "database\characters.db"
 
 # Intents & Bot initialization
 intents = nextcord.Intents.default()
@@ -30,22 +26,6 @@ logger.addHandler(handler)
 @bot.event
 async def on_ready():
     print("\n\tLene#2184 is ready!")
-    if not os.path.exists(db_servers):
-        open(db_servers, "x").close()
-        
-    if not os.path.exists(db_characters):
-        open(db_characters, "x").close()
-    
-    async with aiosqlite.connect(db_servers) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("CREATE TABLE IF NOT EXISTS servers (guild INTEGER)")
-        await db.commit()
-        
-    async with aiosqlite.connect(db_characters) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute("CREATE TABLE IF NOT EXISTS characters (user INTEGER, char_name TEXT, location TEXT)")
-        await db.commit()
-    
     try:
         await bot.sync_application_commands()
         print("\tSynced global commands!")
@@ -59,20 +39,6 @@ async def on_ready():
 async def on_guild_join(guild):
     print(f"Joined server {guild.id}!")
     
-    # Create DB entry
-    async with aiosqlite.connect(db_servers) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute(f"INSERT INTO servers (guild) VALUES ({guild.id})")
-        await db.commit()
-    
-    # Create server sub-directory
-    try:
-        os.mkdir(f"database\id_store\{guild.id}")
-    except FileExistsError:
-        print(f"Folder for {guild.id} already exists, cleaning up & creating a fresh directory...")
-        shutil.rmtree(f"database\id_store\{guild.id}")
-        os.mkdir(f"database\id_store\{guild.id}")
-    
     # Send welcome message
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
@@ -84,19 +50,6 @@ async def on_guild_join(guild):
 @bot.event
 async def on_guild_remove(guild):
     print(f"Left server {guild.id}!")
-    
-    # Delete server sub-directory
-    try:
-        shutil.rmtree(f"database\id_store\{guild.id}")
-    except FileNotFoundError:
-        print(f"PLEASE CHECK: Couldn't find the path specified! (database\id_store\{guild.id})")
-        print(f"DATABASE ENTRY NOT REMOVED! MANUAL REMOVAL MIGHT BE NECESSARY: GUILD ID {guild.id}")
-    
-    # Delete DB entry
-    async with aiosqlite.connect(db_servers) as db:
-        async with db.cursor() as cursor:
-            await cursor.execute(f"DELETE FROM servers WHERE guild={guild.id}")
-        await db.commit()
 
 
 
