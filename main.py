c98ec2371e87784ca3dc133c7166d2f6d5df6940
Bjarne <48617372+SnakeFist007@users.noreply.github.com>
2023-07-23 12:39:20 +0200

    Changed logging date format

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ bot = commands.Bot(intents=intents, help_command=None)
 # * Logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(levelname)s : %(asctime)s - %(message)s",
+    format="%(levelname)s : %(asctime)s - %(message)s", 
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
         logging.FileHandler("jinora-nextcord.log"),
         logging.StreamHandler()
