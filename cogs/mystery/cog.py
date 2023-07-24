eccfe8996acb8880dad34bece9e0c3aadcf73c22
Bjarne <48617372+SnakeFist007@users.noreply.github.com>
2023-07-24 14:46:39 +0200

    Fixed mystery module & added embeds

--- cogs/mystery/cog.py
+++ cogs/mystery/cog.py
@@ -1,9 +1,9 @@
 import nextcord
 import json
 import random
-from nextcord import Interaction, SlashOption
+from nextcord import Interaction, SlashOption, Embed
 from nextcord.ext import commands
-from main import logging, parse_json_utf8
+from main import logging, parse_json_utf8, parse_json
 
 # Initialize Cog
 class Mystery(commands.Cog, name="Mystery"):
@@ -15,16 +15,20 @@ class Mystery(commands.Cog, name="Mystery"):
     async def fortune_cookie(self, interaction: Interaction):
         fortune_cookies = "database/db_fortune.json"
 
-        with open(fortune_cookies, encoding="utf-8") as f:
-            lines = json.load(f)
-
         lines = parse_json_utf8(fortune_cookies)
-        length = len(lines[0])
+        length = len(lines)
 
         rand_int = random.randint(1, length)
         output = lines[str(rand_int)]
 
-        await interaction.response.send_message(f"{output}", ephemeral=True)
+        embed1 = parse_json("database/embeds/standard_embed.json")
+        embed2 = {
+            "title": "Fortune Cookie",
+            "description": f"{output}"
+        }
+        em = Embed().from_dict(embed1 | embed2)
+
+        await interaction.response.send_message(embed=em, ephemeral=True)
 
     # Get an 8-Ball answer for a serious question
     @nextcord.slash_command(name="8ball", description="Answers important questions!")
@@ -32,12 +36,19 @@ class Mystery(commands.Cog, name="Mystery"):
         eight_ball_answers = "database/db_8ball.json"
 
         lines = parse_json_utf8(eight_ball_answers)
-        length = len(lines[0])
+        length = len(lines)
 
         rand_int = random.randint(1, length)
         output = lines[str(rand_int)]
+        
+        embed1 = parse_json("database/embeds/standard_embed.json")
+        embed2 = {
+            "title": "8-Ball Answer",
+            "description": f"{output}"
+        }
+        em = Embed().from_dict(embed1 | embed2)
 
-        await interaction.response.send_message(f"{output}", ephemeral=True)
+        await interaction.response.send_message(embed=em, ephemeral=True)
 
 
 # Add Cog to bot
