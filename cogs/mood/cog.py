f10d11b74d12036a54307500ddf453f331a7cfa4
Bjarne <48617372+SnakeFist007@users.noreply.github.com>
2023-09-15 22:23:39 +0200

    Added mood command with auto-threading

--- cogs/mood/cog.py
+++ cogs/mood/cog.py
@@ -1,5 +1,5 @@
 import nextcord
-from nextcord import Interaction
+from nextcord import Interaction, SlashOption, ChannelType
 from nextcord.ext import commands
 from functions.logging import logging
 
@@ -10,23 +10,17 @@ class Mood(commands.Cog, name="Mood"):
         self.bot = bot
 
     # Mood
+    @commands.has_permissions(create_public_threads=True)
     @nextcord.slash_command(name="mood", description="Not yet implemented!")
-    async def main(self, interaction: Interaction) -> None:
-        pass
-    
-    
-    @main.subcommand(name="log", description="Not yet implemented!")
-    async def meditation_tips(self, interaction: Interaction) -> None:
-        pass
-
-
-    @main.subcommand(name="reminder", description="Not yet implemented!")
-    async def meditation_weekly(self, interaction: Interaction) -> None:
-        pass
-
-
+    async def mood_reminder(self, interaction: Interaction, 
+                            role: nextcord.Role = SlashOption(description="Select a role to ping")) -> None:
+        
+        message = await interaction.send(f"{role.mention} How are you doing today?")
+        msg = await message.fetch()
+        await interaction.channel.create_thread(name=f"☀️", message=msg, type=ChannelType.public_thread)
+        
 
 # Add Cog to bot
 def setup(bot) -> None:
     bot.add_cog(Mood(bot))
-    logging.info("Meditation module loaded!")
+    logging.info("Mood module loaded!")
